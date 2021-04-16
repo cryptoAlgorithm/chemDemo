86570ca84eac2cfd7f9d078dab77a8261acc10a3
vinkwok <CryptoAlgo>
2021-04-16 18:54:21 +0800

    Added second section

---
+++ assets/javascript/main.js
@@ -0,0 +1,7 @@
+const q = (e) => document.querySelector(e);
+const $ = (e) => document.getElementById(e);
+
+window.onscroll = (e) => {
+    if (window.scrollY >= 10) q('header').classList.add('v');
+    else if (window.scrollY < 8) q('header').classList.remove('v');
+}
