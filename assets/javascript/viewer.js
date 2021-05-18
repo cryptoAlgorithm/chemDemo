9d8dc71f5c46324596ec56389d64f1647f8a2182
CryptoAlgo-Dev <cryptoalgro@gmail.com>
2021-05-18 13:59:25 +0800

    Added tab layout

--- assets/javascript/viewer.js
+++ assets/javascript/viewer.js
@@ -7,3 +7,8 @@
     }
     else document.body.classList.add('p__grey');
 })();
+
+const setPage = p => {
+    document.body.classList.remove('s1', 's2', 's3', 's4');
+    document.body.classList.add('s' + p.toString());
+}
