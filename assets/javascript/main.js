54a4523595aa189fcc1580eb09a51db9876a6f89
CryptoAlgo-Dev <cryptoalgro@gmail.com>
2021-05-26 17:37:06 +0800

    Major main page design changes

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,9 +1,11 @@
 const q = (e) => document.querySelector(e);
 const $ = (e) => document.getElementById(e);
 
-window.onscroll = (e) => {
-    if (window.scrollY >= 10) q('header').classList.add('v');
-    else if (window.scrollY < 8) q('header').classList.remove('v');
+const scrollElem = $('scroll');
+
+scrollElem.onscroll = () => {
+    if (scrollElem.scrollTop >= 10) q('header').classList.add('v');
+    else if (scrollElem.scrollTop < 8) q('header').classList.remove('v');
 }
 
 new ThemeHandler($('tBtn'));
